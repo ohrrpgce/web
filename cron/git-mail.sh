d8e665e3c9df4bd5fc95fb9a6832d3002aecc4d2
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2024-10-25 20:44:25 +0000

    Add a script that sends e-mails for new commits on the wip branch
    git-svn-id: https://rpg.hamsterrepublic.com/source/web@13593 7d344553-34f0-0310-a9b1-970ce8f1c3a2

---
+++ cron/git-mail.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# This assumes that we aren't using this particular copy for dev, we treat it as read-only
+# and we just pull new commits on the branch
+
+WORKING_COPY=~/src/ohrcron/ohr-git-mail
+BRANCH=wip
+
+SCRIPTDIR="${0%/*}"
+
+# The smtp_config.sh file can override these variables, and must override PASSWD
+MAILFROM=cron@rpg.hamsterrepublic.com
+MAILTO=ohrrpgce@lists.motherhamster.com
+SMTP="smtps://smtp.dreamhost.com:465"
+USERNAME="cron@rpg.hamsterrepublic.com"
+PASSWD="*REDACTED*"
+
+source "$SCRIPTDIR/smtp_config.sh"
+
+if [ ! -d "$WORKING_COPY" ] ; then
+  echo "Working copy $WORKING_COPY doesn't exist yet, you should create it manually before running this script"
+  exit 1
+fi
+
+cd "$WORKING_COPY"
+git checkout "$BRANCH"
+
+OLD_COMMIT=$(git log | head -1 | cut -d " " -f 2)
+git pull origin "$BRANCH" --rebase
+NEW_COMMIT=$(git log | head -1 | cut -d " " -f 2)
+
+DIFFLOG=$(git log ${OLD_COMMIT}...${NEW_COMMIT})
+
+if [ -z "$DIFFLOG" ] ; then
+  echo "No changes"
+else
+
+  SUBJECT=$(echo "$DIFFLOG" | grep "^ " | tr -s " " | grep -v "^ $" | head -1)
+
+  echo "From: $MAILFROM" > "$SCRIPTDIR/git-mail.txt"
+  echo "To: $MAILTO" >> "$SCRIPTDIR/git-mail.txt"
+  echo "Reply-To: $MAILTO" >> "$SCRIPTDIR/git-mail.txt"
+  echo "Subject: ohrrpgce commit: $SUBJECT" >> "$SCRIPTDIR/git-mail.txt"
+  echo "" >> "$SCRIPTDIR/git-mail.txt"
+  echo "$DIFFLOG" | grep -v "git-svn-id:" >> "$SCRIPTDIR/git-mail.txt"
+
+  curl $SMTP -s -S \
+    --mail-from $MAILFROM \
+    --mail-rcpt $MAILTO \
+    --ssl --ssl-reqd \
+    -u "$USERNAME":"$PASSWD" \
+    --upload-file "$SCRIPTDIR/git-mail.txt"
+  
+fi
