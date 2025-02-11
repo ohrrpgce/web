d02e28d9e99752e83afb5f01f513e717fc08c642
teeemcee <teeemcee@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2025-02-11 23:43:37 +0000

    git-mail.sh: add diffstat, svn rev in subject, and github commit link
    git-svn-id: https://rpg.hamsterrepublic.com/source/web@13771 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- cron/git-mail.sh
+++ cron/git-mail.sh
@@ -35,7 +35,7 @@ OLD_COMMIT=$(git log | head -1 | cut -d " " -f 2)
 git pull origin "$BRANCH" --rebase
 NEW_COMMIT=$(git log | head -1 | cut -d " " -f 2)
 
-DIFFLOG=$(git log ${OLD_COMMIT}...${NEW_COMMIT})
+DIFFLOG=$(git log --stat ${OLD_COMMIT}...${NEW_COMMIT})
 
 if [ -z "$DIFFLOG" ] ; then
   echo "No changes"
@@ -44,29 +44,31 @@ else
   #Split commits
   MAILNUM=1
   printf "$DIFFLOG\x00" | sed  's/^commit /\x00commit /g' | while IFS= read -r -d '' CHUNK ; do
-  
+
     if [ "$CHUNK" = "" ] ; then
       # The first chunk will always be empty
       continue
     fi
 
+    SVNREV=$(echo "$CHUNK" | sed -n -E -e '/git-svn-id:/ s/.*@([0-9]+).*/\1/p')
     SUBJECT=$(echo "$CHUNK" | grep "^ " | tr -s " " | grep -v "^ $" | head -1)
 
     echo "From: $MAILFROM" > "$SCRIPTDIR/git-mail.txt"
     echo "To: $MAILTO" >> "$SCRIPTDIR/git-mail.txt"
     echo "Reply-To: $MAILTO" >> "$SCRIPTDIR/git-mail.txt"
-    echo "Subject: $BRANCH commit:$SUBJECT" >> "$SCRIPTDIR/git-mail.txt"
+    echo "Subject: $BRANCH r$SVNREV:$SUBJECT" >> "$SCRIPTDIR/git-mail.txt"
     echo "" >> "$SCRIPTDIR/git-mail.txt"
-    echo "$CHUNK" | grep -v "git-svn-id:" >> "$SCRIPTDIR/git-mail.txt"
-    
+    echo "$CHUNK" | sed -E -e 's_^commit (\S+)_https://github.com/ohrrpgce/ohrrpgce/commit/\1_' | grep -v "git-svn-id:" >> "$SCRIPTDIR/git-mail.txt"
+
     echo "$MAILNUM Sending mail to $MAILTO -- $SUBJECT"
+
     curl $SMTP -s -S \
       --mail-from $MAILFROM \
       --mail-rcpt $MAILTO \
       --ssl --ssl-reqd \
       -u "$USERNAME":"$PASSWD" \
       --upload-file "$SCRIPTDIR/git-mail.txt"
-    
+
     MAILNUM=$(expr "$MAILNUM" + 1)
 
   done
