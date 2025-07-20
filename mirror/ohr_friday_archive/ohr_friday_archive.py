2d6155d8441876124fa70239b83f4846375f2560
james <james@7d344553-34f0-0310-a9b1-970ce8f1c3a2>
2025-07-20 12:59:23 +0000

    Fix ohr_friday_archive.py cron job to work with python3
    git-svn-id: https://rpg.hamsterrepublic.com/source/web@14002 7d344553-34f0-0310-a9b1-970ce8f1c3a2

--- mirror/ohr_friday_archive/ohr_friday_archive.py
+++ mirror/ohr_friday_archive/ohr_friday_archive.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import os
 import sys
@@ -14,16 +14,16 @@ max_archives = 10
 
 if os.path.exists(destdir):
     if os.path.isdir(destdir):
-        print "Opps! {0} directory already exists.".format(destdir)
+        print("Opps! {0} directory already exists.".format(destdir))
         sys.exit(1)
     else:
-        print "Opps! {0} already exists, but it ain't a directory!".format(destdir)
+        print("Opps! {0} already exists, but it ain't a directory!".format(destdir))
         sys.exit(1)
 
 os.mkdir(destdir)
 
 # copy over the current nightlies
-print "Copying over nightly builds into {0}".format(destdir)
+print("Copying over nightly builds into {0}".format(destdir))
 for filename in os.listdir(nightlies):
     if filename in [".", "..", ".htaccess", "CONFUSED?.html", "ohrrpgce-wip-default.zip"]:
         # skip stuff that we don't want to archive
@@ -54,5 +54,5 @@ dir_list.sort(reverse=True)
 while len(dir_list) > max_archives:
     delete_me = os.path.join(archivedir, dir_list[-1])
     dir_list = dir_list[:-1]
-    print "Deleting old archive {0}".format(delete_me)
+    print("Deleting old archive {0}".format(delete_me))
     shutil.rmtree(delete_me)
